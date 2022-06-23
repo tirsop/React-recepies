5fa9c72ec0afc4585261cdd05c2d01d9489378bf
Tirso Pascual <tpascualgarcia@gmail.com>
2022-06-23 20:22:10 +0900

    basic setup routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom";
+
+// pages
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Recipe from './pages/recipe/Recipe'
+import Search from './pages/search/Search'
+// styles
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipes/:id" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
