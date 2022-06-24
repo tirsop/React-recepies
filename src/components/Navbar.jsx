3ee66357d96e054b12eb3b581f02298a76573709
Tirso Pascual <tpascualgarcia@gmail.com>
2022-06-24 16:25:04 +0900

    change title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
   return (
     <div className='navbar'>
       <nav>
-        <Link to="/" className='brand'><h1>Cooking Ninja</h1></Link>
+        <Link to="/" className='brand'><h1>Spanish food recipes</h1></Link>
         <Searchbar />
         <Link to="/create">Create Recipe</Link>
       </nav>
