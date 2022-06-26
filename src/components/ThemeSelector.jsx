89caa2f6a452db2fe2e4038bd540179d3733e1b1
Tirso Pascual <tpascualgarcia@gmail.com>
2022-06-26 10:46:40 +0900

    toggle dark/light mode setup

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -1,15 +1,26 @@
 import { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
+import modeIcon from '../assets/mode-icon.svg'
 // styles
 import './ThemeSelector.css'
 
 const themeColors = ['#58249c', '#249c6b', '#b70233']
 
 export default function ThemeSelector() {
-  const { changeColor } = useContext(ThemeContext)
+  const { changeColor, changeMode, mode } = useContext(ThemeContext)
+
+  const toggleMode = () => {
+    changeMode(mode === 'dark' ? 'light' : 'dark')
+  }
 
   return (
     <div className='theme-selector'>
+      <div className="mode-toggle">
+        <img src={modeIcon} alt="dark/light toggle icon"
+          onClick={toggleMode}
+          style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)' }}
+        />
+      </div>
       <div className="theme-buttons">
         {themeColors.map(color => (
           <div
