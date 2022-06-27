af64f50c5f61c6e24d598d6dc94ec37538c7faea
Tirso Pascual <tpascualgarcia@gmail.com>
2022-06-27 10:25:09 +0900

    fetch data from firebase in the show page

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,17 +1,36 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { ThemeContext } from "../../context/ThemeContext"
-import { useFetch } from '../../hooks/useFetch'
+import { db } from "../../firebase/config"
+import { doc, getDoc } from "firebase/firestore"
+// import { useFetch } from '../../hooks/useFetch'
 
 
 // styles
 import './Recipe.css'
 
 export default function Recipe() {
+  const { mode } = useContext(ThemeContext)  // fot styling the dark mode
   const { id } = useParams()
-  const url = `http://localhost:3000/recipes/${id}`
-  const { data: recipe, isPending, error } = useFetch(url)
-  const { mode } = useContext(ThemeContext)
+  // const url = `http://localhost:3000/recipes/${id}`
+  // const { data: recipe, isPending, error } = useFetch(url)
+  const [recipe, setRecipe] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true)
+    getDoc(doc(db, 'recipes', id))
+      .then(doc => {
+        if (doc.exists()) {
+          setIsPending(false)
+          setRecipe(doc.data())
+        } else {
+          setIsPending(false)
+          setError("Could not find that recipe")
+        }
+      })
+  }, [id])
 
   return (
     <div className={`recipe ${mode}`}>
