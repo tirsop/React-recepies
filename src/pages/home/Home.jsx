a82dfd5cc03e6552e02cf0f3937d4da78a67b594
Tirso Pascual <tpascualgarcia@gmail.com>
2022-06-27 14:55:54 +0900

    add real-time db

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 // import { useFetch } from '../../hooks/useFetch'
 import { useContext, useEffect, useState } from 'react'
 import { ThemeContext } from '../../context/ThemeContext'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, onSnapshot } from 'firebase/firestore'
 import { db } from '../../firebase/config'
 // components
 import RecipeList from '../../components/RecipeList'
@@ -9,36 +9,64 @@ import RecipeList from '../../components/RecipeList'
 import './Home.css'
 
 export default function Home() {
+  const { mode } = useContext(ThemeContext)  // to style dark mode
+
+  // // when using json-server, fetch the data with the line below
   // const { data: recipes, isPending, error } = useFetch('http://localhost:3000/recipes')
-  const [recipes, setRecipes] = useState(null)
+
+  // when using firebase
+  const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const [error, setError] = useState(false)
 
+  // // without real-time data
+  // useEffect(() => {
+  //   setIsPending(true)
+  //   getDocs(collection(db, 'recipes'))
+  //     .then(snapshot => {
+  //       if (snapshot.empty) {
+  //         setError("No recipes to load")
+  //         setIsPending(false)
+  //       } else {
+  //         // console.log(snapshot.docs[0].data());
+  //         let results = []
+  //         snapshot.docs.forEach(doc => {
+  //           results.push({ id: doc.id, ...doc.data() })
+  //         })
+  //         setRecipes(results)
+  //         setIsPending(false)
+  //       }
+  //     })
+  // }, [])
+
+  // with real-time
+  const col = 'recipes'
   useEffect(() => {
     setIsPending(true)
-    getDocs(collection(db, 'recipes'))
-      .then(snapshot => {
-        if (snapshot.empty) {
-          setError("No recipes to load")
-          setIsPending(false)
-        } else {
-          // console.log(snapshot.docs[0].data());
-          let results = []
-          snapshot.docs.forEach(doc => {
-            results.push({ id: doc.id, ...doc.data() })
-          })
-          setRecipes(results)
-          setIsPending(false)
-        }
-      })
-  }, [])
-  const { mode } = useContext(ThemeContext)  // to style dark mode
+    const unsub = onSnapshot(collection(db, col), (snapshot) => {
+      if (snapshot.empty) {
+        setError(`No ${col} to load.`)
+        setIsPending(false)
+      } else {
+        // console.log(snapshot.docs[0].data());
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({ id: doc.id, ...doc.data() })
+        })
+        setData(results)
+        setIsPending(false)
+      }
+    })
+
+    return () => unsub()
+  }, [col])
+
 
   return (
     <div className='home'>
       {error && <p className={`error ${mode}`}>{error}</p>}
       {isPending && <p className='loading'>Loading.........</p>}
-      {recipes && <RecipeList recipes={recipes} />}
+      {data && <RecipeList recipes={data} />}
     </div>
   )
 }
